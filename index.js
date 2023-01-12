449af3a1abebda09c727a2ee4d65ac835807c3b1
Dre <dredawkins11@gmail.com>
2023-01-12 16:20:23 -0600

    finished

--- index.js
+++ index.js
@@ -1,13 +1,86 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "title",
+        message: "What is the title of your project?",
+        type: "input",
+    },
+    {
+        name: "description",
+        message: "What is the description of your project?",
+        type: "input",
+    },
+    {
+        name: "installation",
+        message: "What are the installation steps for your project?",
+        type: "input",
+    },
+    {
+        name: "usage",
+        message: "What are the usage instructions for your project?",
+        type: "input",
+    },
+    {
+        name: "contribution",
+        message: "What are the details on the contributions to this project?",
+        type: "input",
+    },
+    {
+        name: "testing",
+        message: "Describe the tests that can be ran, and how to run them.",
+        type: "input",
+    },
+    {
+        name: "license",
+        message: "Which license is your project under?",
+        type: "list",
+        choices: [
+            "MIT",
+            "Apache 2.0",
+            "GNU 3.0",
+            "Eclipse Public",
+            "Boost Software 1.0",
+            "Creative Commons Zero 1.0",
+        ],
+    },
+    {
+        name: "gitHub",
+        message: "What is your GitHub username?",
+        type: "input",
+    },
+    {
+        name: "email",
+        message: "What is your email?",
+        type: "input",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`./${fileName}`, data, (err) => console.log())
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    const answers = await inquirer.prompt(questions);
+    const answerStrings = [
+        `# *${answers.title}*\n`,
+        "## Table of Contents\n- [Description](#Description)\n- [Installation](#Installation)\n- [Usage](#Usage)\n- [Contributions](#Contributions)\n- [Testing](#Testing)\n\n",
+        `## Description\n${answers.description}`,
+        `## Installation Steps\n${answers.installation}`,
+        `## Usage Instructions\n${answers.usage}`,
+        `## Contributions\n${answers.contribution}`,
+        `## Testing Information\n${answers.testing}`,
+        `## License\nLicensed under the ${answers.license} license.`,
+        `## Contact Info\n[**GitHub**](www.github.com/${answers.github})<br>[**${answers.email}**]()`
+    ];
+
+    
+    writeToFile("result.md", answerStrings.join("\n\n"))
+}
 
 // Function call to initialize app
 init();
---
+++ result.md
@@ -0,0 +1,32 @@
+# *Project 1*
+
+
+## Table of Contents
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contributions](#Contributions)
+- [Testing](#Testing)
+
+
+
+## Description
+This is my first project.
+
+## Installation Steps
+These are the steps to install my project: 1. Install it
+
+## Usage Instructions
+Run the .exe file
+
+## Contributions
+Me, Myselft, and I
+
+## Testing Information
+There are no tests.
+
+## License
+Licensed under the MIT license.
+
+## Contact Info
+[**GitHub**](www.github.com/undefined)<br>[**dredawkins11@gmail.com**]()
